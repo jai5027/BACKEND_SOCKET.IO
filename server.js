1bee4bbf153f229763eb281c9d20ca5d16324d16
Jai Sharma <jaisharma27868@gmail.com>
2026-04-05 00:18:46 +0530

    socket.io impliment

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const app = require('./src/app.js')
+const server = require('./src/app.js')
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log("server is running on port 3000")
 })
--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
+const http = require('http')
+const path = require('path')
 const express = require('express');
 
 const app = express()
+app.use(express.json())
 
-module.exports = app
+app.use(express.static(path.resolve("./public")))
+const server = http.createServer(app)
+
+app.get('/', (req, res) => {
+    return res.sendFile("/public/index.html")
+})
+
+module.exports = server
