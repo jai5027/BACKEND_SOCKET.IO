c6be84ea45ce1d73eda15519c15388ef1bf371f3
Jai Sharma <jaisharma27868@gmail.com>
2026-04-05 08:50:58 +0530

    socket.io frontend

--- server.js
+++ server.js
@@ -3,3 +3,4 @@ const server = require('./src/app.js')
 server.listen(3000, () => {
     console.log("server is running on port 3000")
 })
+
--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const http = require('http')
 const path = require('path')
 const express = require('express');
+const { Server } = require("socket.io")
 
 const app = express()
 app.use(express.json())
-
 app.use(express.static(path.resolve("./public")))
 const server = http.createServer(app)
+const io = new Server(server)
+
+io.on("connection", (client) => {
+    console.log("New user connected", Socket.id)
+})
 
 app.get('/', (req, res) => {
     return res.sendFile("/public/index.html")
